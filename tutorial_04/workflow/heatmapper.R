8d3c3af10ab7b630248527a9609aca8c2bd99fa7
RobMeulenkamp <lrmeulenkamp@bioinf.nl>
2022-03-17 13:34:06 +0100

    added assignment 4

---
+++ tutorial_04/workflow/heatmapper.R
@@ -0,0 +1,11 @@
+args = commandArgs(trailingOnly=TRUE)
+
+jpeg(args[2])
+d <- as.matrix(read.csv(args[1], header=FALSE, sep=",")[-1,-1])
+rownames(d) <- read.csv(args[1], header=FALSE, sep=",")[-1,1]
+colnames(d) <- read.csv(args[1], header=FALSE, sep=",")[1,-1]
+
+heatmap(d)
+
+dev.off()
+
